b2b18866f89bddd431a4e1ce3fe7d62e1d54fa5d
= <thattinhkiemphai.07@gmail.com>
2016-05-20 21:37:33 +0700

    hi

--- MyProject/Controllers/AdvisingPageController.cs
+++ MyProject/Controllers/AdvisingPageController.cs
@@ -11,7 +11,7 @@ namespace MyProject.Controllers
         // GET: AdvisingPage
         public ActionResult Index()
         {
-            return View();
+            return View(); 
         }
     }
 }
